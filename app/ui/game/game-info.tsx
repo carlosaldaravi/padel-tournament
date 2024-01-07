b05d3367f442b4a6d755578db2636281f855ec48
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2024-01-07 03:41:43 +0000

    add svg

--- app/ui/game/game-info.tsx
+++ app/ui/game/game-info.tsx
@@ -1,9 +1,12 @@
 import { formatDate } from "@/app/lib/dates";
+import SVG from "../svg";
 
 const GameInfo = ({ court, date }: { court?: string; date?: Date }) => {
   return (
     <div className="w-full border-b flex justify-between gap-4 p-2">
-      <span>{court !== "" ? court : "--"}</span>
+      <span className="flex gap-1 lg:gap-2 items-center">
+        <SVG type="court" size="6" /> {court !== "" ? court : "--"}
+      </span>
       <span className="text-end">{date ? formatDate(date) : "--"}</span>
     </div>
   );
